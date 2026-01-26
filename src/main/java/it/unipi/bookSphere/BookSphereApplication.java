0c10050476caa5fbb73c74a686405d4a17e6bfe4
matteobilliciani <m.billiciani@studenti.unipi.it>
2026-01-26 10:36:43 +0100

    project creation

---
+++ src/main/java/it/unipi/bookSphere/BookSphereApplication.java
@@ -0,0 +1,13 @@
+package it.unipi.bookSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unipi/bookSphere/BookSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.bookSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
