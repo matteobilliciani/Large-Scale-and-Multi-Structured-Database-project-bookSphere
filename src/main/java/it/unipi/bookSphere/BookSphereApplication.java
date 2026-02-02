4b72677478a99dea87b1f82f0d36c2805e72b33f
matteobilliciani <m.billiciani@studenti.unipi.it>
2026-02-02 12:53:30 +0100

    main e tests

--- src/main/java/it/unipi/bookSphere/BookSphereApplication.java
+++ src/main/java/it/unipi/bookSphere/BookSphereApplication.java
@@ -1,7 +1,13 @@
 package it.unipi.bookSphere;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BookSphereApplication {
 
-	public static void main(String [] args) {}
+	public static void main(String[] args) {
+		SpringApplication.run(BookSphereApplication.class, args);
+	}
 
 }
--- src/test/java/it/unipi/bookSphere/BookSphereApplicationTests.java
+++ src/test/java/it/unipi/bookSphere/BookSphereApplicationTests.java
@@ -2,8 +2,10 @@ package it.unipi.bookSphere;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"wsl", "test-connection"})
 class BookSphereApplicationTests {
 
 	@Test
